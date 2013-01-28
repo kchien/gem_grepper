c44206f1a1984c917be91846139d7077d2620dbc
Steven Haddox <steven@haddox.us>
2013-01-28 13:28:59 -0500

    Add `get` help instructions

--- features/gem_grepper.feature
+++ features/gem_grepper.feature
@@ -6,4 +6,9 @@ Feature: gem_grepper help
   Scenario: App outputs usage instructions
     When I successfully run `gem_grepper`
     Then the exit status should be 0
-    And the output should include useful instructions
+    And the output should include usage instructions
+
+  Scenario: App outputs `get` usage instructions
+    When I successfully run `gem_grepper help get`
+    Then the exit status should be 0
+    And the output should include `get` usage instructions
--- features/steps/gem_grepper_help.rb
+++ features/steps/gem_grepper_help.rb
@@ -2,20 +2,36 @@ require 'aruba/api'
 class GemGrepperHelp < Spinach::FeatureSteps
   include Aruba::Api
 
-  When 'I successfully run `gem_grepper`' do
-    run "gem_grepper help"
-  end
-
   Then 'the exit status should be 0' do
     assert_exit_status(0)
   end
 
-  And 'the output should include useful instructions' do
+  When 'I successfully run `gem_grepper`' do
+    run "gem_grepper help"
+  end
+
+  When 'I successfully run `gem_grepper help get`' do
+    run "gem_grepper help get"
+  end
+
+  And 'the output should include usage instructions' do
     assert_passing_with("NAME")
     assert_passing_with("gem_grepper - Gather information about other gems from multiple sources")
     assert_passing_with("SYNOPSIS")
-    assert_passing_with("-f, --file")
     assert_passing_with("--help")
     assert_passing_with("--version")
+    assert_passing_with("COMMANDS")
+    assert_passing_with("export - Output gem data in a specified format")
+    assert_passing_with("get    - Retrieve data about specified gems")
+    assert_passing_with("help   - Shows a list of commands or help for one command")
+  end
+
+  And 'the output should include `get` usage instructions' do
+    assert_passing_with("NAME")
+    assert_passing_with("get -")
+    assert_passing_with("SYNOPSIS")
+    assert_passing_with("'comma-delimited,list,of,gems'")
+    assert_passing_with("COMMAND OPTIONS")
+    assert_passing_with("-f, --file")
   end
 end
